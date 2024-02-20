bda88396e722c6ce0d44732a63f34ec58331cd55
KarlBorromeo <98805305+KarlBorromeo@users.noreply.github.com>
2024-02-21 00:34:23 +0800

    updated index html

---
+++ SQL_updated3.sql
@@ -0,0 +1,118 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema ecommerce
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema ecommerce
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `ecommerce` DEFAULT CHARACTER SET utf8mb3 ;
+USE `ecommerce` ;
+
+-- -----------------------------------------------------
+-- Table `ecommerce`.`products`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ecommerce`.`products` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `description` TEXT NULL DEFAULT NULL,
+  `category` VARCHAR(255) NOT NULL,
+  `price` INT NOT NULL,
+  `stocks` INT NOT NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `ecommerce`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ecommerce`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(255) NOT NULL,
+  `lastname` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `role` VARCHAR(45) NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `ecommerce`.`cart_items`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ecommerce`.`cart_items` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `products_id` INT NOT NULL,
+  `pieces` INT NOT NULL,
+  `users_id` INT NOT NULL,
+  `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_cart_items_products1_idx` (`products_id` ASC) VISIBLE,
+  INDEX `fk_cart_items_users1_idx` (`users_id` ASC) VISIBLE,
+  CONSTRAINT `fk_cart_items_products1`
+    FOREIGN KEY (`products_id`)
+    REFERENCES `ecommerce`.`products` (`id`),
+  CONSTRAINT `fk_cart_items_users1`
+    FOREIGN KEY (`users_id`)
+    REFERENCES `ecommerce`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `ecommerce`.`images`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ecommerce`.`images` (
+  `products_id` INT NOT NULL,
+  `url` TEXT NULL DEFAULT NULL,
+  INDEX `fk_images_products1_idx` (`products_id` ASC) VISIBLE,
+  CONSTRAINT `fk_images_products1`
+    FOREIGN KEY (`products_id`)
+    REFERENCES `ecommerce`.`products` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `ecommerce`.`orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ecommerce`.`orders` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `status` VARCHAR(255) NOT NULL,
+  `order_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  `address` VARCHAR(255) NOT NULL,
+  `address2` VARCHAR(255) NULL DEFAULT NULL,
+  `city` VARCHAR(255) NOT NULL,
+  `state` VARCHAR(255) NOT NULL,
+  `zip_code` VARCHAR(255) NOT NULL,
+  `cart_items_id` INT NOT NULL,
+  `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  INDEX `fk_orders_cart_items1_idx` (`cart_items_id` ASC) VISIBLE,
+  CONSTRAINT `fk_orders_cart_items1`
+    FOREIGN KEY (`cart_items_id`)
+    REFERENCES `ecommerce`.`cart_items` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
